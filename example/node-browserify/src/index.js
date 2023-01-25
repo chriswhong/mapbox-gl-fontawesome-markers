ebe98dcbf90397d34a25ccf69188d5149bf1eba6
Chris Whong <chris.m.whong@gmail.com>
2023-01-24 22:51:26 -0500

    remove browserify example

--- example/node-browserify/src/index.js
+++
@@ -1,4 +0,0 @@
-window.mapboxgl = require('mapbox-gl')
-window.FontawesomeMarker = require('../../../dist/index')
-require('../node_modules/@fortawesome/fontawesome-free/js/all.min.js')
-require('../../common/map-with-markers.js')
